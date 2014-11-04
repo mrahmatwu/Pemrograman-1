01b7c11ad84bc9a7b04332eb400ca5d4b1b6c978
mrahmatwu <m.rahmatwu@gmail.com>
2014-11-04 20:57:58 +0700

    Commit Kedua

---
+++ Prak7_Pemrog1_Moh Rachmat W_4513210025/rahmatsatua.java
@@ -0,0 +1,11 @@
+public class rahmatsatua
+{
+public static void main(String [] args) {
+		for (int line = 1; line <= 9; line++) {
+			for (int j = 1; j <= line; j++) {
+				System.out.print(line);
+			}
+			System.out.println();
+		}
+	}
+	}
